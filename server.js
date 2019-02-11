22c8ce71645043c6c3a35d4bebd40532f0b37646
Zerojame <toypoyomn03@outlook.com>
2019-02-11 10:55:15 +0700

    Add server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', function (req, res) {
+	res.send('chatbot-nodejs-heroku-starter!!');
+})
+
+app.set('port', (process.env.PORT || 4000))
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
+
